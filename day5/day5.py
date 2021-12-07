f9f3feb90a78b1f191d918724aeb0ab2b67c535c
emperorpickles <emperor.pickles@gmail.com>
2021-12-07 19:22:27 +0000

    day five part two solve

--- day5/day5.py
+++ day5/day5.py
@@ -2,36 +2,28 @@
 Day 5: Hydrothermal Venture
 '''
 
+import math
 import numpy as np
 
 
-def update_field(pt_1, pt_2, field):
+def update_field(pts, field):
     '''Returns field after updating with given line'''
-    start_x, start_y = min(pt_1[0], pt_2[0]), min(pt_1[1], pt_2[1])
-    end_x, end_y = max(pt_1[0], pt_2[0]), max(pt_1[1], pt_2[1])
+    slope = get_slope(pts[0], pts[1])
 
-    if pt_1[0] == pt_2[0]:
-        for i in range(start_y, end_y + 1):
-            field[start_x][i] += 1
-    elif pt_1[1] == pt_2[1]:
-        for i in range(start_x, end_x + 1):
-            field[i][start_y] += 1
-    else:
-        # diagonals - not working
-        x_dist = pt_2[0] - pt_1[0]
-        y_dist = pt_2[1] - pt_1[1]
+    pts = sorted(pts, key=(lambda x: x[0]) if slope is not None else (lambda x: x[1]))
+    pt_1, pt_2 = pts[0], pts[1]
+    dist = abs(pt_1[0] - pt_2[0]) if slope is not None else abs(pt_1[1] - pt_2[1])
 
-        x_dir = 1 if x_dist > 0 else -1
-        y_dir = 1 if y_dist > 0 else -1
+    for i in range(dist + 1):
+        x_offset = i if slope is not None else 0
+        y_offset = slope if slope is not None else int(
+            math.copysign(1, (pt_2[1] - pt_1[1])))
+        field[pt_1[0] + x_offset][pt_1[1] + (i * y_offset)] += 1
 
-        x_range = range(0, x_dist + x_dir, x_dir)
-        y_range = range(0, y_dist + y_dir, y_dir)
 
-        print(pt_1)
-
-        for i in x_range:
-            field[pt_1[0] + i][pt_1[1] +
-                               (i * y_dir if x_dir != -1 else i)] += 1
+def get_slope(pt_1, pt_2):
+    '''Returns the slope of the line between two points. Returns None if line is vertical'''
+    return int((pt_2[1] - pt_1[1]) / (pt_2[0] - pt_1[0])) if (pt_2[0] - pt_1[0]) else None
 
 
 def part_one(vent_lines):
@@ -40,10 +32,8 @@ def part_one(vent_lines):
     field = np.zeros((size + 1, size + 1))
 
     for line in vent_lines:
-        # [[ 72 504], [422 154]]
-        start, end = line[0], line[1]
-        if start[0] == end[0] or start[1] == end[1]:
-            update_field(start, end, field)
+        if line[0][0] == line[1][0] or line[0][1] == line[1][1]:
+            update_field(line, field)
     print(len(field[field > 1]))
 
 
@@ -53,10 +43,8 @@ def part_two(vent_lines):
     field = np.zeros((size + 1, size + 1))
 
     for line in vent_lines:
-        # [[ 72 504], [422 154]]
-        start, end = line[0], line[1]
-        update_field(start, end, field)
-    # update_field([72, 504], [422, 154], field)
+        update_field(line, field)
+
     np.savetxt('field.txt', field, fmt='%d')
     print(len(field[field > 1]))
 
---
+++ template.py
@@ -0,0 +1,37 @@
+'''
+Day X:
+'''
+
+import numpy as np
+
+
+def part_one():
+    '''
+    Prints the number of fish after x number of days
+
+    Only works for part one, array becomes too large and slow to handle more than ~170 days
+    '''
+
+
+def part_two():
+    '''
+    Prints the number of fish after x number of days
+
+    Main idea found via reddit
+    '''
+
+
+def main(input_file):
+    '''Read input file and call helper functions'''
+    with open(input_file, 'r', encoding='utf8') as file:
+        lines = []
+        for line in file:
+            lines.append([i.split(',') for i in line.strip().split(' -> ')])
+    lines = np.loadtxt(input_file, dtype=np.uint8, delimiter=',')
+
+    part_one()
+    part_two()
+
+
+if __name__ == '__main__':
+    main('input.txt')
